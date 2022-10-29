ad2e94236e87f902ded3028a03393a41e625a339
Mangesh1k <69412541+Mangesh1k@users.noreply.github.com>
2022-10-29 20:35:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function updateclcok(){
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-    let ampm = "AM"
+    //
     if(h > 12){
         h = h - 12
         ampm = "PM"
